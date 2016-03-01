394426f260245031d2bd97865055d6c9d559202d
shazhenhua <shazhenhua@tsjinrong.cn>
2016-03-01 09:50:32 +0800

    Creat XX

--- src/cn/kila/com/kila/Car.java
+++ src/cn/kila/com/kila/Car.java
@@ -1,7 +1,12 @@
 package cn.kila.com.kila;
 
 public class Car {
-public void maned(){
+       public void maned(){
 	System.out.println("汽车可以载人！");
 }
+       public int XX(){
+    	   int i = 0;
+		return i;
+    	   
+       }
 }
